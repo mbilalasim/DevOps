09ac03bbd88485609cbdc4da3b6e9975391204c1
Muhammad Bilal <bilal.asim5555@outlook.com>
2024-01-11 12:21:45 +0500

    message

---
+++ gitConfig.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Muhammad Bilal"
+git config --global user.email "bilal.asim5555@outlook.com"
+git config --global core.editor "code --wait"
+git config --global core.autocrlf true
+git config --global diff.tool "vscode"
+git config --global difftool.vscode.cmd "code --wait --diff $LOCAL $REMOTE"
